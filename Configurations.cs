aafb3792a3c3a8c835d5c0e911af8e9b377d70e3
Romulo Freitas <rfcontatosvia@gmail.com>
2023-06-30 19:20:53 -0300

    Autenticação e Autorização (apiSecurity)

---
+++ Configurations.cs
@@ -0,0 +1,7 @@
+namespace Blog;
+
+public static class Configuration
+{
+    //TOKEN - JWT - Json Web Token
+    public static string JwKey {get; set;} = "d41d8cd98f00b204e9800998ecf8427e";
+}
---
+++ Services/TokenService.cs
@@ -0,0 +1,12 @@
+using Blog.Models;
+
+namespace Blog.Services;
+
+public class TokenServices
+{
+    public string GenerateToken(User user)
+    {
+
+    }
+}
+
