885219eb277dafcf91614fbbc2ebb611b71a8d4a
Romulo Freitas <rfcontatosvia@gmail.com>
2023-07-03 20:48:35 -0300

    Criação do método GenerateToken [Autenticação e Autorização]

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,4 +1,7 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Blog.Services;
 
@@ -6,7 +9,16 @@ public class TokenServices
 {
     public string GenerateToken(User user)
     {
-
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configuration.JwKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddHours(8),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+        
     }
 }
 
