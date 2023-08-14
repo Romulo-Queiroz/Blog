f024bbfd2edd6621fae43126242ae40d3f2c8f24
Romulo Freitas <rfcontatosvia@gmail.com>
2023-08-14 19:54:32 -0300

    fix errors

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -50,12 +50,37 @@ public class AccountController : ControllerBase
             return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
         }
     }
-    [HttpPost("v1/login")]
-   public IActionResult Login([FromServices]TokenServices tokenService)
-   {
-  
-    var token = tokenService.GenerateToken(null);
-    return Ok(token);
-   }
+     [HttpPost("v1/accounts/login")]
+    public async Task<IActionResult> Login(
+        [FromBody] LoginViewModel model,
+        [FromServices] BlogDataContext context,
+        [FromServices] TokenServices tokenService)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<string>(
+                ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage).ToList()
+            ));
 
+        var user = await context
+            .Users
+            .AsNoTracking()
+            .Include(x => x.Roles)
+            .FirstOrDefaultAsync(x => x.Email == model.Email);
+
+        if (user == null)
+            return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
+
+        if (!PasswordHasher.Verify(user.PasswordHash, model.Password))
+            return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
+
+        try
+        {
+            var token = tokenService.GenerateToken(user);
+            return Ok(new ResultViewModel<string>(token, null));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
+        }
+    }
 }
