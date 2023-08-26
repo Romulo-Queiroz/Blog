295ea7b1295d187ad110c57b3a32bad3dea3e0bf
Romulo Freitas <rfcontatosvia@gmail.com>
2023-08-26 16:37:36 -0300

    Feat: Paginação dos posts

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -10,11 +10,16 @@ namespace Blog.Controllers;
 [ApiController]
 public class PostController : ControllerBase
 {
-	[HttpGet("v1/posts")]
-	public async Task<IActionResult> GetAsync(
-		[FromServices]BlogDataContext context)
-	{
-        var posts = await context
+    [HttpGet("v1/posts")]
+    public async Task<IActionResult> GetAsync(
+        [FromServices] BlogDataContext context,
+        [FromQuery] int page,
+        [FromQuery] int pageSize = 25)
+    {
+        try
+        {
+            var count = await context.Posts.AsNoTracking().CountAsync();
+            var posts = await context
                 .Posts
                 .AsNoTracking()
                 .Include(x => x.Author)
@@ -28,7 +33,21 @@ public class PostController : ControllerBase
                     Category = x.Category.Name,
                     Author = $"{x.Author.Name} ({x.Author.Email})"
                 })
-            .ToListAsync();
-		return Ok(posts);
-	}
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToListAsync();
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                total = count,
+                page,
+                pageSize,
+                posts
+            }));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<List<Category>>("05X04 - Falha interna no servidor"));
+        }
+    }
 }
